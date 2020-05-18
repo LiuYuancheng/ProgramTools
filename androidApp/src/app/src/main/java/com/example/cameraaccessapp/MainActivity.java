d9bb0c34010df5e0be014c91d4b475ce749b93d8
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2020-05-18 09:55:23 +0800

    add camera take photo.

--- androidApp/src/app/src/main/java/com/example/cameraaccessapp/MainActivity.java
+++ androidApp/src/app/src/main/java/com/example/cameraaccessapp/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.cameraaccessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.cameraaccessapp.MESSAGE";
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
+    Intent cameraIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
         // Set the Activity title by getting a string from the Resources object, because
         //  this method requires a CharSequence rather than a resource ID
         getWindow().setTitle(getResources().getText(R.string.app_name));
+        imageView = (ImageView) this.findViewById(R.id.imageView1);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+            }
+        });
 
     }
 
@@ -30,6 +51,15 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void onActivityResult(View view) {
+        System.out.println("123123");
+
+
+        Bitmap photo = (Bitmap) cameraIntent.getExtras().get("data");
+        imageView.setImageBitmap(photo);
+
     }
 
 }
